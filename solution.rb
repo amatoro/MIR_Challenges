2d5183cccce993bde1ec695fa81b535adf46c0c4
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-02-23 18:46:42 -0500

    MIR | Sinatra | Challenge Three

--- solution.rb
+++ solution.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 
-get '/' do
-  nombre = params[:nombre]
+# get '/' do
+#   nombre = params[:nombre]
 
-  if request.query_string.empty? || nombre.empty?
-    "<h1>Hola desconocido!</h1>"
-  else
-    "<h1>Hola #{nombre.capitalize}!</h1>"
-  end  
+#   if request.query_string.empty? || nombre.empty?
+#     "<h1>Hola desconocido!</h1>"
+#   else
+#     "<h1>Hola #{nombre.capitalize}!</h1>"
+#   end  
 
-end
+# end
 
 get '/makers/' do
   "<h1>Hola desconocido!</h1>" #Diferencia aquí entre usar caputalize con o sin !
@@ -18,4 +18,15 @@ end
 
 get '/makers/:nombre' do
   "<h1>Hola #{params['nombre'].capitalize}!</h1>" #Diferencia aquí entre usar caputalize con o sin !
+end
+
+get '/' do 
+  erb :home
+
+  # <<- HTML #Headdoc? - string muy largo
+  # HTML
+end
+
+post '/hola' do
+  "¡Hola #{params['nombre']}!"
 end
