ee6bd9fc6c1015779cae7f8372ff046d0d2a1784
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-02-23 00:44:43 -0500

    MIR | Sinatra | Challenge Two

--- solution.rb
+++ solution.rb
@@ -6,7 +6,16 @@ get '/' do
   if request.query_string.empty? || nombre.empty?
     "<h1>Hola desconocido!</h1>"
   else
-    "<h1>Hola #{nombre}!</h1>"
+    "<h1>Hola #{nombre.capitalize}!</h1>"
   end  
 
+end
+
+get '/makers/' do
+  "<h1>Hola desconocido!</h1>" #Diferencia aquí entre usar caputalize con o sin !
+end
+
+
+get '/makers/:nombre' do
+  "<h1>Hola #{params['nombre'].capitalize}!</h1>" #Diferencia aquí entre usar caputalize con o sin !
 end
