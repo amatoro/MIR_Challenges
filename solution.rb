61134902730dab6e394e0aa8bbe156d67b73b6fd
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-02-24 19:27:18 -0500

    MIR | Sinatra | Challenge Five. Solution

--- solution.rb
+++ solution.rb
@@ -1,34 +1,8 @@
 require 'sinatra'
 
-# get '/' do
-#   nombre = params[:nombre]
-
-#   if request.query_string.empty? || nombre.empty?
-#     "<h1>Hola desconocido!</h1>"
-#   else
-#     "<h1>Hola #{nombre.capitalize}!</h1>"
-#   end  
-
-# end
-
-get '/makers/' do
-  "<h1>Hola desconocido!</h1>" 
-end
-
-
-get '/makers/:nombre' do
-  "<h1>Hola #{params['nombre'].capitalize}!</h1>" 
-end
-
 get '/' do 
-  "HcUy6Re2LLBRtj"
 
-  # <<- HTML #Headdoc? - string muy largo
-  # HTML
+  erb :list
+
+
 end
-
-post '/hola' do
-  "¡Hola #{params['nombre']}!"
-end
-
-
